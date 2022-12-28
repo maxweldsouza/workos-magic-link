ccb4e18ac928306bc4417f7277c3bb2f3019597c
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2022-12-28 12:38:57 +0530

    feat: Add redirect uri

--- pages/api/magic.js
+++ pages/api/magic.js
@@ -9,6 +9,7 @@ export default async (req, res) => {
     const session = await workos.passwordless.createSession({
         email: req.body.email,
         type: 'MagicLink',
+        redirectURI: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/callback`
     });
 
     res.json({
