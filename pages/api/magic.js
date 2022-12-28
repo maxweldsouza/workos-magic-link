eae35955455897f4fcd21e4e27e0b4abde26b21c
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2022-12-28 12:44:04 +0530

    feat: Handle errors

--- pages/api/magic.js
+++ pages/api/magic.js
@@ -4,15 +4,18 @@ const workos = new WorkOS(process.env.WORKOS_API_KEY);
 
 export default async (req, res) => {
     if (req.method !== 'POST') return res.status(405)
-    console.log('req.body.email', req.body.email)
+    try {
+        const session = await workos.passwordless.createSession({
+            email: req.body.email,
+            type: 'MagicLink',
+            redirectURI: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/callback`
+        });
 
-    const session = await workos.passwordless.createSession({
-        email: req.body.email,
-        type: 'MagicLink',
-        redirectURI: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/callback`
-    });
+        res.json({
+            redirect: `/check-email?link=${session.link}`
+        });
 
-    res.json({
-        redirect: `/check-email?link=${session.link}`
-    });
+    } catch (e) {
+        res.status(500).json(e)
+    }
 };
